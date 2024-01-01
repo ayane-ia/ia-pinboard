efa9e589bd95ec76f6799b9d35c831567c4d01be
Yrin10k <nayrton383@gmail.com>
2024-01-01 03:46:38 -0300

    correcao de bus

--- config/config.php
+++ config/config.php
@@ -10,4 +10,4 @@ define('CONTROLLER_PADRAO', 'home');
 define('METODO_PADRAO', 'index');
 define('NAMESPACE_CONTROLLER', 'app\\controllers\\');
 
-define('URL_BASE', 'http://localhost/projetos/iapinboard/');
+define('URL_BASE', 'http://localhost/projetos/ia-pinboard/');
--- index.php
+++ index.php
@@ -1,4 +1,14 @@
 <?php
+if(!file_exists(".htaccess")){
+    $temp = fopen(".htaccess", "w");
+    if(!$temp) die("HOUVE ERRO NA CRIACAO DE ARQUIVOS DE SERVIDOR");
+    fwrite($temp, "
+    RewriteEngine On
+    RewriteCond %{REQUEST_FILENAME} !-f
+    RewriteCond %{REQUEST_FILENAME} !-d
+    RewriteRule ^(.*)$ index.php/$1 [L]");
+    fclose($temp);
+}
 require 'config/config.php';
 require 'app/core/Core.php';
 require 'vendor/autoload.php';
