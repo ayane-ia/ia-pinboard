2f8aa617d2b6582ee3bfd2e34a5248b99d24ce57
Ayano <148707134+Ayano-Oficial@users.noreply.github.com>
2024-01-04 18:45:06 -0300

    add

--- app/views/home.php
+++ app/views/home.php
@@ -1,10 +1,9 @@
-
 <div class="titulo-mobile">
       <h1 class="titulo-m styleb-text-center styleb-mt-3">Tudo</h1>
     </div>
     <div class="container-mae">
       
-      <div class="grid-container">
+    <div class="grid-container">
       <?php if($imagens){ foreach($imagens as $img){?>
         <div class="grid-item">
             <a href="#"><img class="lazyload"  data-src="<?php echo URL_BASE."userData/".$img->image_path?>" alt="Image" loading="lazy"></a>
@@ -13,6 +12,9 @@
         <!-- Adicione mais itens de grade conforme necessário -->
       </div>
     </div>
-    
+        <!-- Adicione mais itens de grade conforme necessário -->
+      </div>
+    </div>
+
 <script src="https://cdnjs.cloudflare.com/ajax/libs/lazysizes/5.3.2/lazysizes.min.js" integrity="sha512-q583ppKrCRc7N5O0n2nzUiJ+suUv7Et1JGels4bXOaMFQcamPk9HjdUknZuuFjBNs7tsMuadge5k9RzdmO+1GQ==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
 <?php ?>
