2dc5b3847ad5e5cebc38ffd93965b469f867826b
Ayano <148707134+Ayano-Oficial@users.noreply.github.com>
2024-01-04 19:09:37 -0300

    rem

--- config/config.php
+++ config/config.php
@@ -11,4 +11,4 @@ define('METODO_PADRAO', 'index');
 define('NAMESPACE_CONTROLLER', 'app\\controllers\\');
 
 define('URL_BASE', 'http://localhost/projeto/ia-pinboard/');
-define('USER_PATH', 'C:\xampp\htdocs\projeto\ia-pinboard\userData');
+define('USER_PATH', 'C:\xampp\htdocs\projeto\ia-pinboard\userData\\');
