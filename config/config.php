f2866e483dbc65d9281a2907040154e2f192472f
Ayron Nascimento <156387838+ayronnasc@users.noreply.github.com>
2024-01-24 19:09:24 -0300

    CHANGES

--- config/config.php
+++ config/config.php
@@ -10,6 +10,6 @@ define('CONTROLLER_PADRAO', 'home');
 define('METODO_PADRAO', 'index');
 define('NAMESPACE_CONTROLLER', 'app\\controllers\\');
 
-define('URL_BASE', 'http://ia-pinboard.shop/');
+define('URL_BASE', 'http://localhost/projetos/ia-pinboard/');
 define('USER_PATH', 'C:\xampp\htdocs\projetos\ia-pinboard\userData\\');
 define('PATH_BASE', 'C:\xampp\htdocs\projetos\ia-pinboard\\');
